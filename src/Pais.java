809fcd16489a5fe86bfc47a514e6bcb6f9f51f4a
anacmaciel <casmaciel96@gmail.com>
2022-07-14 21:40:08 -0300

    Subindo parte da proposta da lista007

--- src/Pais.java
+++ src/Pais.java
@@ -1,74 +1,72 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
-public class Pais {
+public class Pais       {
+     private String codigoIso;
+     private  String nome;
+     private int populacao;
+     private double dimensao;
+     Set<Pais> listaDePaisesComFronteira = new HashSet<Pais>();
 
-    private String codigoIso;
-    private String nomePais;
-    private double populacao;
-    private double dimensao;
-    List<Pais> fronteiraDePaises = new ArrayList();
+     public Set<Pais> getListaDePaisesComFronteira() {
+          return listaDePaisesComFronteira;
+     }
 
-    public Pais(String codigoIso, String nomePais, double populacao, double dimensao, List<Pais> fronteiraDePaises) {
-        this.codigoIso = codigoIso;
-        this.nomePais = nomePais;
-        this.populacao = populacao;
-        this.dimensao = dimensao;
-        ;
-    }
+     public Pais(String codigoIso, String nome, int populacao, double dimensao) {
+          this.codigoIso = codigoIso;
+          this.nome = nome;
+          this.populacao = populacao;
+          this.dimensao= dimensao;
+     }
 
-    public String getCodigoIso() {
-        return codigoIso;
-    }
+     public String getCodigoIso() {
+                    return codigoIso;
+     }
 
-    public void setCodigoIso(String codigoIso) {
-        this.codigoIso = codigoIso;
-    }
+     public void setCodigoIso(String codigoIso) {
+          this.codigoIso = codigoIso;
+     }
 
-    public String getNomePais() {
-        return nomePais;
-    }
+     public String getNome() {
+          return nome;
+     }
 
-    public void setNomePais(String nomePais) {
-        this.nomePais = nomePais;
-    }
+     public void setNome(String nome) {
+          this.nome = nome;
+     }
 
-    public double getPopulacao() {
-        return populacao;
-    }
+     public int getPopulacao() {
+          return populacao;
+     }
 
-    public void setPopulacao(double populacao) {
-        this.populacao = populacao;
-    }
+     public void setPopulacao(int populacao) {
+          this.populacao = populacao;
+     }
 
-    public double getDimensao() {
-        return dimensao;
-    }
+     public double getDimensao() {
+          return dimensao;
+     }
 
-    public void setDimensao(double dimensao) {
-        this.dimensao = dimensao;
-    }
+     public void setDimensao(double dimensao) {
+          this.dimensao = dimensao;
+     }
 
-    public List<Pais> getFronteiraDePaises() {
-        return fronteiraDePaises;
-    }
+     public void adicionarPaiscomFronteira(Pais p) {
+          listaDePaisesComFronteira.add(p);
+     }
 
-    public void setFronteiraDePaises(List<Pais> fronteiraDePaises) {
-        this.fronteiraDePaises = fronteiraDePaises;
-    }
+     public void fazFronteira(Pais p) {
+          if (listaDePaisesComFronteira.contains(p.getNome())) {
+               System.out.println(p.getNome() + " faz fronteira com " + this.getNome());
 
-    public void adicionarFronteiraDePaises(Pais pais) {
-        fronteiraDePaises.add(pais);
-    }
+          } else {
+               System.out.println(p.getNome() + " não faz fronteira com " + this.getNome());
+          }
+     }
 
-    public boolean fronteiraDePaises(String fronteira) {
-        for (int i = 0; I < fronteiraDePaises.size(); i++) {
-            if (fronteiraDePaises.get(i).getNomePais().equals(fronteira)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
+          public  double calcularDencidade() {
+               return  populacao / dimensao;
+          }
 
 }
+
