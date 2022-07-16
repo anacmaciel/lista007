b4ee270c56edef4f564e19d79763a5815631b7f2
anacmaciel <casmaciel96@gmail.com>
2022-07-16 18:22:36 -0300

    subindo alteracao no projeto, removendo linhas em branco

--- src/MainPais.java
+++ src/MainPais.java
@@ -1,7 +1,6 @@
 public class MainPais {
-    public static void main(String[] args) {
+cd docu      public static void main(String[] args) {
         Pais brasil = new Pais("BRA", "Brasil", 10000, 5000.0);
-
         Pais estadosUnidos = new Pais("USA", "Estados Unidos da América", 8000, 4000.0);
 
         Pais mexico = new Pais("MX", "México",  7000, 3000.0);
